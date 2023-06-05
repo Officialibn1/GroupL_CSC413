58cee901b8c18984fdb6fea3fe85652cef15fc7e
Officialibn1 <officialibn001@gmail.com>
2023-06-05 23:58:22 +0100

    Website updated

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ open.addEventListener('click', togglerNavbar);
 let close = document.getElementById('close');
 
 close.addEventListener('click', togglerNavbar);
+
+var navLlink = document.getElementById('link')
+
+navLlink.addEventListener('click', toggleNav)
+
+var navLlink2 = document.getElementById('link2')
+
+navLlink2.addEventListener('click', toggleNav)
+
+var navLlink3 = document.getElementById('link3')
+
+navLlink3.addEventListener('click', toggleNav)
+
+var navLlink4 = document.getElementById('link4')
+
+navLlink4.addEventListener('click', toggleNav)
+
+var navLlink5 = document.getElementById('link5')
+
+navLlink5.addEventListener('click', toggleNav)
